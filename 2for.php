78b15c255ffeae1d019264eb3e75811bc86ed33e
Podzalupenko Ochkovich <andrey5hard@gmail.com>
2025-03-01 12:19:32 +0300

    fix

--- 2for.php
+++ 2for.php
@@ -2,7 +2,7 @@
 for ($i = 1; $i <= 100; $i++) {
     $prostoe = true;
     for ($j = 2; $j < $i; $j++) {
-        if ($i % $j == 0) {
+        if ($i % $j === 0) {
             $prostoe = false;
             break;
         }
